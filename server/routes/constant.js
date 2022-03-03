1c20d2cccab6cc9b4fa5b46175ac725230a7c5a5
Vigneshwar <prasannavenkatesh@colanonline.com>
2022-03-03 13:08:45 +0530

    constant page comment updated,

--- server/routes/constant.js
+++ server/routes/constant.js
@@ -1,6 +1,9 @@
-module.exports={
-     MY_ACCOUNT_ID:"0.0.30792091",
-     MY_PRIVATE_KEY:"302e020100300506032b657004220420bd41459bc3edec3554049a719e3a1c6272bff4a3006cf7d77f4bbcb23f658177",
-     API_SECRET_KEY:"dfdgh347dfnhh$$%%%%%33657667&%#$^&fdgfhgfhghgh4445yhsb@@@&*",
-     TokenName:"jwtprasannatoken"
+module.exports = {
+     // Acount ID and Private key from hedera account
+     MY_ACCOUNT_ID: "0.0.30792091",
+     MY_PRIVATE_KEY: "302e020100300506032b657004220420bd41459bc3edec3554049a719e3a1c6272bff4a3006cf7d77f4bbcb23f658177",
+
+     API_SECRET_KEY: "dfdgh347dfnhh$$%%%%%33657667&%#$^&fdgfhgfhghgh4445yhsb@@@&*",
+
+     TokenName: "jwtprasannatoken"
 }
